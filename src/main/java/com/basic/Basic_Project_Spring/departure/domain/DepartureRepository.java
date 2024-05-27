e03c8f93a8956f15c05f7cf9d112ef3f8f866709
awj1052 <awj1052@naver.com>
2024-05-28 02:33:44 +0900

    fix: DepartureRepository

--- src/main/java/com/basic/Basic_Project_Spring/departure/domain/DepartureRepository.java
+++ src/main/java/com/basic/Basic_Project_Spring/departure/domain/DepartureRepository.java
@@ -1,17 +1,19 @@
 package com.basic.Basic_Project_Spring.departure.domain;
 
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface DepartureRepository extends JpaRepository<Departure, Long> {
 
     List<Departure> findAllByDeparturesContainingOrArrivalsContaining(String keyword1, String keyword2);
     List<Departure> findAllByDate(LocalDate date);
 
-    @Query("SELECT s FROM Ship s WHERE s.date = :date AND (s.departures LIKE :keyword OR s.arrivals LIKE :keyword)")
-    List<Departure> findAllByDateAndQuery(LocalDate date, String keyword);
+    @Query("SELECT s FROM Departure s WHERE s.date = :date AND (s.departures LIKE :keyword OR s.arrivals LIKE :keyword)")
+    List<Departure> findAllByDateAndQuery(@Param("date") LocalDate date, @Param("keyword") String keyword);
 }
 
 //    String string = "2019-01-10";
