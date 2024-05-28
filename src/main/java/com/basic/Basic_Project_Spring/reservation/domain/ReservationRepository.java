1b20fefbfbf41078542931f39c17850f5240b6dc
awj1052 <awj1052@naver.com>
2024-05-28 20:44:25 +0900

    fix: transactional

--- src/main/java/com/basic/Basic_Project_Spring/reservation/domain/ReservationRepository.java
+++ src/main/java/com/basic/Basic_Project_Spring/reservation/domain/ReservationRepository.java
@@ -2,12 +2,16 @@ package com.basic.Basic_Project_Spring.reservation.domain;
 
 import com.basic.Basic_Project_Spring.departure.domain.Departure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @Transactional
     void deleteAllByDeparture(Departure departure);
+
     List<Reservation> findAllByMemberId(Long memberId);
+
     List<Reservation> findAllByDepartureId(Long departureId);
 }
--- src/main/java/com/basic/Basic_Project_Spring/ship/domain/ShipRepository.java
+++ src/main/java/com/basic/Basic_Project_Spring/ship/domain/ShipRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ShipRepository extends JpaRepository<Ship, Long> {
+
     List<Ship> findAllByOwner(Member owner);
 }
